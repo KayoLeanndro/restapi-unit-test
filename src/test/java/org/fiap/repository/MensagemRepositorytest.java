9bad5f2974877c830116b5e423dd1951eca908da
KayoLeanndro <kayoleanndro2@gmail.com>
2026-08-15 13:05:15 -0300

    test: Criando Teste Unitario no Repository test e implementando mockito

--- src/test/java/org/fiap/repository/MensagemRepositorytest.java
+++ src/test/java/org/fiap/repository/MensagemRepositorytest.java
@@ -1,7 +1,46 @@
 package org.fiap.repository;
 
+import org.fiap.model.Mensagem;
+import org.fiap.model.Usuario;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class MensagemRepositorytest {
 
+    @Mock
+    private MensagemRepository mensagemRepository;
 
+    @Test
+    public void devePermitirRegistrarMensagem(){
+        var mensagem = gerarMensagem();
+
+        //Arrange - Preparar
+        //Quando o repository salvar qualquer objeto do tipo mensagem deve retornar o objeto Mensagem
+        when(mensagemRepository.save(any(Mensagem.class))).thenReturn(mensagem);
+
+        //Act - Atuar
+        var mensagemArmazena = mensagemRepository.save(mensagem);
+    }
+
+    private Mensagem gerarMensagem(){
+        Mensagem mensagem = new Mensagem();
+        mensagem.setId(UUID.randomUUID());
+        mensagem.setConteudo("Um Post Sobre Cabras");
+        mensagem.setUsuario(gerarUsuario());
+
+        return mensagem;
+    }
+
+    private Usuario gerarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNome("Ruth Camile");
+
+        return usuario;
+    }
 
 }
